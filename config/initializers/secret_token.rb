60914234c7154b190d92ba11bbfe369775d7e542
gregrails3tutorial <gregrails3tutorial@gmail.com>
2010-11-22 21:53:16 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'a5297dc34edee2edd2bbb622a154e9a312dd0cb088153929d32fb06648cd58106d3b765540aab3fc7b722380722dcd25d1d4e677e82c20a6c3f3b4d06f388359'
